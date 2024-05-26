995810d645ec0f10f847504ceb9bdcc70316986e
Macailh <salvadorenriquegb@gmail.com>
2024-05-25 19:39:39 -0600

    Add vue-chartjs and chartjs-plugin-datalabels integration example

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <BarChart />
+</template>
+
+<script>
+import BarChart from './components/BarChart.vue'
+// import ChartJsPluginDataLabels from 'chartjs-plugin-datalabels';
+
+export default {
+  name: 'App',
+  components: { BarChart }
+}
+</script>
+<st
---
+++ src/components/BarChart.vue
@@ -0,0 +1,28 @@
+<template>
+  <Bar
+    id="my-chart-id"
+    :options="chartOptions"
+    :data="chartData"
+  />
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { Bar } from 'vue-chartjs';
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
+import ChartJsPluginDataLabels from 'chartjs-plugin-datalabels';
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, ChartJsPluginDataLabels);
+
+const chartData = ref({
+  labels: ['January', 'February', 'March'],
+  datasets: [{ data: [40, 20, 12] }],
+});
+
+const chartOptions = ref({
+  responsive: true,
+});
+</script>
+
+<style scoped>
+</style>
